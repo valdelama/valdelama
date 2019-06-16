67e4d56887a99f3647ffe7f39c78188205a4cd13
Daniel Winer <dantarifa@gmail.com>
2019-06-16 13:09:18 +0200

    Fix job spacing

--- src/components/Job.js
+++ src/components/Job.js
@@ -3,14 +3,16 @@ import BodyCopy from 'src/components/BodyCopy'
 import styles from './Job.module.scss'
 
 export default ({ children, company, dates, role }) => (
-  <BodyCopy className={styles.wrapper}>
-    <div className={styles.company}>{company}</div>
-    <div className={styles.content}>
-      <div className={styles.meta}>
-        <span className={styles.role}>{role}</span>
-        <span className={styles.dates}>{dates}</span>
+  <BodyCopy>
+    <div className={styles.wrapper}>
+      <div className={styles.company}>{company}</div>
+      <div className={styles.content}>
+        <div className={styles.meta}>
+          <span className={styles.role}>{role}</span>
+          <span className={styles.dates}>{dates}</span>
+        </div>
+        <div className={styles.description}>{children}</div>
       </div>
-      <div className={styles.description}>{children}</div>
     </div>
   </BodyCopy>
 )
